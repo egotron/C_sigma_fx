9ec67b653d08f307b18e9c5e83810b96e0597b4e
Egotron <egotron@hotmail.com>
2020-10-26 19:08:14 +0900

    Add files via upload

---
+++ part_1/c_sigma_fx_part_1_06/c_sigma_fx_part_1_06.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+void	expression_relational()
+{
+	int i = 1;
+	int j = 2;
+
+	printf("[ relational expression ]\n");
+	printf("i = %d, j = %d\n", i, j);
+	printf("i < j = %d\n", i < j);
+	printf("i > j = %d\n", i > j);
+	printf("i <= j = %d\n", i <= j);
+	printf("i >= j = %d\n", i >= j);
+	printf("i == j = %d\n", i == j);
+	printf("i != j = %d\n", i != j);
+}
+
+void	main(void)
+{
+	expression_relational();
+}
