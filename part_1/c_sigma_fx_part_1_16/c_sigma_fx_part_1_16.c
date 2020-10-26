9a041b188e1600f268f0aa58f5a9abd0a405610d
Egotron <egotron@hotmail.com>
2020-10-26 19:17:36 +0900

    Add files via upload

---
+++ part_1/c_sigma_fx_part_1_16/c_sigma_fx_part_1_16.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int		function_sum(int from, int to)
+{
+	int sum = 0;
+
+	printf("[ function sum ]\n");
+	printf("from = %d, to = %d\n", from, to);
+
+	for (int count = from; ; )
+	{
+		sum += count;
+		if (count++ < to)	continue;
+
+		goto exit;
+	}
+	
+exit:
+	printf("sum = %d\n", sum);
+	
+	return sum;
+}
+
+void	main(void)
+{
+	function_sum(1, 10);
+}
