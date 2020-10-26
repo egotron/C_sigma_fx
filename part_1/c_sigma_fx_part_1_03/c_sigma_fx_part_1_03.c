2184fcee4c5193651c35af11d603d59dcd3660d4
Egotron <egotron@hotmail.com>
2020-10-26 19:05:12 +0900

    Add files via upload

---
+++ part_1/c_sigma_fx_part_1_03/c_sigma_fx_part_1_03.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+void	expression_unary()
+{
+	int i = 1;
+	int* pi = &i;
+
+	printf("[ unary expression ]\n");
+	printf("i = %d\n", i);
+	printf("&i = %p\n", &i);
+	printf("*pi = %d\n", *pi);
+	printf("~i = %d\n", ~i);
+	printf("!i = %d\n", !i);
+	i++;
+	printf("i++ = %d\n", i);
+	--i;
+	printf("--i = %d\n", --i);
+}
+
+void	main(void)
+{
+	expression_unary();
+}
