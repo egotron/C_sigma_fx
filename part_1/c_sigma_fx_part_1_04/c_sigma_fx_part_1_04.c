2f96b4407269de6b5258ca96d1195ebfa145255c
Egotron <egotron@hotmail.com>
2020-10-26 19:06:26 +0900

    Add files via upload

---
+++ part_1/c_sigma_fx_part_1_04/c_sigma_fx_part_1_04.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+void	expression_arithmetic()
+{
+	int i = 1;
+	int j = 2;
+
+	printf("[ arithmetic expression ]\n");
+	printf("i + j = %d\n", i + j);
+	printf("i - j = %d\n", i - j);
+	printf("i * j = %d\n", i * j);
+	printf("i / j = %d\n", i / j);
+	printf("i %% j = %d\n", i % j);
+}
+
+void	main(void)
+{
+	expression_arithmetic();
+}
