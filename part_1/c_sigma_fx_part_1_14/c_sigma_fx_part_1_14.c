fd896c1b32b21b348e84e88dec6700751d017642
Egotron <egotron@hotmail.com>
2020-10-26 19:15:39 +0900

    Add files via upload

---
+++ part_1/c_sigma_fx_part_1_14/c_sigma_fx_part_1_14.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+void	statement_do_while()
+{
+	int count = 0;
+	int sum;
+
+	sum = 0;
+
+	printf("[ statement do while ]\n");
+	printf("count = %d, sum = %d\n", count, sum);
+
+	do {
+		count++;
+		sum += count;
+	} while (count < 10);
+
+	printf("count = %d, sum = %d\n", count, sum);
+}
+
+void	main(void)
+{
+	statement_do_while();
+}
