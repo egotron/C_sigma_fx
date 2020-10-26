496d94c70b2f0e1948804dad5c4f6f437fb286a7
Egotron <egotron@hotmail.com>
2020-10-26 19:09:04 +0900

    Add files via upload

---
+++ part_1/c_sigma_fx_part_1_07/c_sigma_fx_part_1_07.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+void	expression_bitwise()
+{
+	int i = 0x3;
+	int j = 0x1;
+
+	printf("[ bitwise expression ]\n");
+	printf("i = %02X, j = %02X\n", i, j);
+	printf("i & j = %02X\n", i & j);
+	printf("i | j = %02X\n", i | j);
+	printf("i ^ j = %02X\n", i ^ j);
+}
+
+void	main(void)
+{
+	expression_bitwise();
+}
