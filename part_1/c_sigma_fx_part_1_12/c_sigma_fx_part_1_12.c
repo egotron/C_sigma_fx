872f805fa53a5889f60bcf73ae0c65bdd8a6d2cc
Egotron <egotron@hotmail.com>
2020-10-26 19:13:51 +0900

    Add files via upload

---
+++ part_1/c_sigma_fx_part_1_12/c_sigma_fx_part_1_12.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+void	statement_switch()
+{
+	int i = 1;
+	int j = 2;
+	int max;
+
+	printf("[ statement switch ]\n");
+	printf("i = %d, j = %d\n", i, j);
+
+	switch (i)
+	{
+	case 0: max = 0; break;
+	case 1:
+		max = 1;
+		j = max;
+		break;
+	case 2:
+		{
+			max = 2;
+			j = max;
+		}
+		break;
+	default:
+		max = -1;
+		j = 0;
+	}
+
+	printf("i = %d, j = %d, max = %d\n", i, j, max);
+}
+
+void	main(void)
+{
+	statement_switch();
+}
