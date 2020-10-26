08366e224a84865ffed52400b8568bceabb99f71
Egotron <egotron@hotmail.com>
2020-10-26 19:10:58 +0900

    Add files via upload

---
+++ part_1/c_sigma_fx_part_1_09/c_sigma_fx_part_1_09.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+void	expression_conditional()
+{
+	int i = 1;
+	int j = 2;
+
+	printf("[ conditional expression ]\n");
+	printf("i = %d, j = %d\n", i, j);
+	printf("i < j ? %s\n", i < j ? "true" : "false");
+	printf("(i > j) ? %s\n", (i > j) ? "true" : "false");
+}
+
+void	main(void)
+{
+	expression_conditional();
+}
