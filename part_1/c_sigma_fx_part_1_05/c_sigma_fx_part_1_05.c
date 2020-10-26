b5cde8a0b12470772944b590b2d80f993abf3b03
Egotron <egotron@hotmail.com>
2020-10-26 19:07:23 +0900

    Add files via upload

---
+++ part_1/c_sigma_fx_part_1_05/c_sigma_fx_part_1_05.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+void	expression_bit_shift()
+{
+	int i = 2;
+
+	printf("[ bit shift expression ]\n");
+	printf("i << 1 = %d\n", i << 1);
+	printf("i >> 1 = %d\n", i >> 1);
+}
+
+void	main(void)
+{
+	expression_bit_shift();
+}
