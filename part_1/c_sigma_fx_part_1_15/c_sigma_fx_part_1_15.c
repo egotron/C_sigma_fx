f07b78042b282b41ee87e07e0707ba33e0c46a88
Egotron <egotron@hotmail.com>
2020-10-26 19:16:44 +0900

    Add files via upload

---
+++ part_1/c_sigma_fx_part_1_15/c_sigma_fx_part_1_15.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+void	statement_for()
+{
+	int sum = 0;
+
+	printf("[ statement for ]\n");
+	printf("sum = %d\n", sum);
+
+	for (int count = 1; count <= 10; count++)
+	{
+		sum += count;
+	}
+
+	printf("sum = %d\n", sum);
+}
+
+void	main(void)
+{
+	statement_for();
+}
