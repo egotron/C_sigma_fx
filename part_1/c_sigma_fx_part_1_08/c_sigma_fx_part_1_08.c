e996f1b1175b945ea87819a9f02d791816a6be47
Egotron <egotron@hotmail.com>
2020-10-26 19:10:01 +0900

    Add files via upload

---
+++ part_1/c_sigma_fx_part_1_08/c_sigma_fx_part_1_08.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+void	expression_logical()
+{
+	int i = 1;
+	int j = 0;
+
+	printf("[ logical expression ]\n");
+	printf("i = %d, j = %d\n", i, j);
+	printf("i && j = %d\n", i && j);
+	printf("i || j = %d\n", i || j);
+}
+
+void	main(void)
+{
+	expression_logical();
+}
