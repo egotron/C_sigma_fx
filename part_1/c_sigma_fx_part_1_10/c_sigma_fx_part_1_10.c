11d6d8687a3fc46222e6bc42eb3d26121af5329d
Egotron <egotron@hotmail.com>
2020-10-26 19:11:49 +0900

    Add files via upload

---
+++ part_1/c_sigma_fx_part_1_10/c_sigma_fx_part_1_10.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+void	expression_assignment()
+{
+	int i = 1;
+	int j = 2;
+
+	printf("[ assignment expression ]\n");
+	printf("i = %d, j = %d\n", i, j);
+	printf("i *= j = %d\n", i *= j);
+	printf("i /= j = %d\n", i /= j);
+	printf("i %%= j = %d\n", i %= j);
+	printf("i += j = %d\n", i += j);
+	printf("i -= j = %d\n", i -= j);
+	printf("i <<= j = %d\n", i <<= j);
+	printf("i >>= j = %d\n", i >>= j);
+	printf("i &= j = %d\n", i &= j);
+	printf("i ^= j = %d\n", i ^= j);
+	printf("i |= j = %d\n", i |= j);
+}
+
+void	main(void)
+{
+	expression_assignment();
+}
